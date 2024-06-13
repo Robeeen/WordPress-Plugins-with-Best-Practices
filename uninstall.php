2bd8bb9a9a4cd6bf63b4ce3cb1a32dd3fc904540
Robeeen <robeeen@gmail.com>
2024-06-13 14:34:51 +0600

    uninstall issues fixing

--- uninstall.php
+++ uninstall.php
@@ -1,10 +1,21 @@
 <?php
 
 // if uninstall.php is not called by WordPress, die
-if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
-    die;
+
+register_deactivation_hook( __FILE__, 'deactivate_plugin' );
+
+function deactivate_plugin($category_id) {
+
+    $args = array(
+        'category' => $category_id,
+        'numberposts' => -1, // Fetch all posts
+        'post_status' => 'any' // Include drafts, published, etc.
+    );
+
+    $total_posts = get_posts($args);
+    foreach($total_posts as $post){
+        wp_delete_post($post->ID, true);
+    }
+
 }
-
-$option_name = 'new_form';
-
-delete_option( $option_name );
+deactivate_plugin(31);
